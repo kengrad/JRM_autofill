f208f49a56708aa2316ba70145e14a9943d6e536
kengrad <xkenig@gmail.com>
2023-03-22 20:24:11 +0400

    add watcher_fill

--- test_autofill.py
+++ test_autofill.py
@@ -4,17 +4,17 @@ from pages.report_fill_JRM import *
 from pages.report_fill_watcher import report_fill_watcher
 
 
-# def test_jrm_autofill(page):
-#     page.goto("http://10.39.3.226:8000/form/")
-#     auth(page)
-#     auth2(page)
-#     if date_start:
-#         date_change(page)
-#     all_selects(page)
-#     coment_fill(page)
-#     issue_and_risks_fill(page)
-#     submit(page)
-#     page.wait_for_timeout(10000)
+def test_jrm_autofill(page):
+    page.goto("http://10.39.3.226:8000/form/")
+    auth(page)
+    auth2(page)
+    if date_start:
+        date_change(page)
+    all_selects(page)
+    coment_fill(page)
+    issue_and_risks_fill(page)
+    submit(page)
+    page.wait_for_timeout(10000)
 
 
 def test_watcher_autofill(page):
